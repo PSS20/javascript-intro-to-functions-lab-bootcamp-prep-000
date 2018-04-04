6df9a0e0fd84b9cc65259faac2f17aab1a0ca4f7
Poonam <poonamsodani09@gmail.com>
2018-04-04 05:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ function longWhisper(text1){
 }
 
 function sayHiToGrandma(text){
-  if(text.toLowerCase === text){
+  if(text.toLowerCase() === text){
     return "I can't hear you!"
+  }
+  elseif(text.toUpperCase() === text){
+    return "YES INDEED"
+  }
+  if(text === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
